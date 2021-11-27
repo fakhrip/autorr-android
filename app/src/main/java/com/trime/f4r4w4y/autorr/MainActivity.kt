c8778f55082875ac1d6a37671f79e88fd057b500
fakhrip <fakhriputra123s@gmail.com>
2021-11-27 11:58:46 +0700

    initialize repo

---
+++ app/src/main/java/com/trime/f4r4w4y/autorr/MainActivity.kt
@@ -0,0 +1,162 @@
+package com.trime.f4r4w4y.autorr
+
+import android.app.Application
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var sViewModel: SensorViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val sViewModel: SensorViewModel = ViewModelProvider(this)[SensorViewModel::class.java]
+        sViewModel.registerSensors()
+        sViewModel.setup()
+    }
+
+    class SensorViewModel(application: Application) : AndroidViewModel(application),
+        SensorEventListener {
+        private lateinit var sensorManager: SensorManager
+        private lateinit var gyroscope: Sensor
+        private lateinit var accelerometer: Sensor
+
+        private var gravity = FloatArray(3) { 0F }
+        private var accValue = FloatArray(3) { 0F }
+        private var gyrValue = FloatArray(3) { 0F }
+
+        fun registerSensors() {
+            sensorManager =
+                getApplication<Application>().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+            sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)?.let {
+                this.gyroscope = it
+            } ?: run {
+                Snackbar.make(
+                    View(getApplication<Application>().applicationContext),
+                    "Your device have no gyroscope sensors !!!",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }
+
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.let {
+                this.accelerometer = it
+            } ?: run {
+                Snackbar.make(
+                    View(getApplication<Application>().applicationContext),
+                    "Your device have no accelerometer sensors !!!",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }
+
+            sensorManager.registerListener(
+                this,
+                this.gyroscope,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+
+            sensorManager.registerListener(
+                this,
+                this.accelerometer,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+        }
+
+        fun unregisterSensors() {
+            sensorManager.unregisterListener(this)
+        }
+
+        fun setup() {
+            viewModelScope.launch {
+                Log.d("SENSOR_R", "counting ...")
+                val results = fetchResults()
+                Log.d("SENSOR_R", "result: ${results.contentDeepToString()}")
+            }
+        }
+
+        private suspend fun fetchResults(): Array<FloatArray> {
+            delay(1000) // Weirdly needed, to let the sensor register itself first
+            val time = System.currentTimeMillis()
+            var lastSeconds = String.format("%.2f", -1F).toFloat()
+
+            var timeArr: FloatArray = floatArrayOf()
+            var accX: FloatArray = floatArrayOf()
+            var accY: FloatArray = floatArrayOf()
+            var accZ: FloatArray = floatArrayOf()
+            var gyrX: FloatArray = floatArrayOf()
+            var gyrY: FloatArray = floatArrayOf()
+            var gyrZ: FloatArray = floatArrayOf()
+
+            while (accX.size != 20) {
+                delay(1) // Weirdly needed, to let the sensor update the value first
+                val seconds =
+                    String.format("%.2f", (System.currentTimeMillis() - time) / 1000F).toFloat()
+                if (!"%.2f".format(lastSeconds).contentEquals("%.2f".format(seconds))) {
+                    lastSeconds = seconds
+
+                    timeArr = timeArr.plus(seconds)
+                    accX = accX.plus(accValue[0])
+                    accY = accY.plus(accValue[1])
+                    accZ = accZ.plus(accValue[2])
+                    gyrX = gyrX.plus(gyrValue[0])
+                    gyrY = gyrY.plus(gyrValue[0])
+                    gyrZ = gyrZ.plus(gyrValue[0])
+                }
+            }
+
+            return arrayOf(timeArr, accX, accY, accZ, gyrX, gyrY, gyrZ)
+        }
+
+        override fun onSensorChanged(event: SensorEvent) {
+            if (event.sensor.type == Sensor.TYPE_GYROSCOPE) {
+                gyrValue[0] = event.values[0]
+                gyrValue[1] = event.values[1]
+                gyrValue[2] = event.values[2]
+            } else if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                // In this example, alpha is calculated as t / (t + dT),
+                // where t is the low-pass filter's time-constant and
+                // dT is the event delivery rate.
+
+                val alpha = 0.8f
+
+                // Isolate the force of gravity with the low-pass filter.
+                gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0]
+                gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1]
+                gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2]
+
+                // Remove the gravity contribution with the high-pass filter.
+                accValue[0] = event.values[0] - gravity[0]
+                accValue[1] = event.values[1] - gravity[1]
+                accValue[2] = event.values[2] - gravity[2] - gravity[2]
+            }
+
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+            // Do nothing
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sViewModel.unregisterSensors()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
